8ad6296a26a4ca0585b78a39334c2aaf9f79f9a7
Juan Pinto <juanca1795@gmail.com>
2021-05-11 14:14:52 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root',/* selector - nombre que el componente va a tener*/
+  templateUrl: './app.component.html', /* Contraparte de html */
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  /* title: string = 'app contador'
+  numero: number = 10
+  base: number = 50
+
+  sumar(){
+    this.numero += 1
+  }
+
+  resta(){
+    this.numero -= 1
+  }
+
+  acumular(valor: number){
+    this.numero += valor
+  }
+ */
+
+
+
 }
